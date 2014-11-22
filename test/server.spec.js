215c59c182eebf6fe55e1a1853c042a20ca04834
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-11-22 17:24:24 +0300

    Use socket.io-client for integration testing

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,9 +1,12 @@
 var assert = require('assert');
+var client = require('socket.io-client')('http://localhost:8080');
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(5));
+var server = require('../src/server.js');
+
+describe('server', function () {
+  it('accepts connections', function (done) {
+    client.emit('login', 'user1', function () {
+      done();
     });
-  });
-});
+  })
+})
