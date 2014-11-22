791322ede82b84560ceafd1216458468c2725f75
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-11-22 20:21:10 +0300

    Test that the receiver is notified about the message

--- test/server.spec.js
+++ test/server.spec.js
@@ -37,6 +37,32 @@ describe('server', function () {
     })
   });
 
+  it('notifies the receiver about the message', function (done) {
+    var client1 = io(url, options);
+    var client2 = io(url, options);
+
+    client1.emit('login', {id: 'user1'}, function () {
+      client1.emit('startSession', 'user2', function () {
+        client2.emit('login', {id: 'user2'}, function () {
+          client2.emit('startSession', 'user1', function () {
+            client1.emit('message', {
+              to: 'user2',
+              text: 'Message text'
+            })
+            client2.on('message', function (message) {
+              assert.equal(message.from, 'user1');
+              assert.equal(message.to, 'user2');
+              assert.equal(message.text, 'Message text');
+              client1.disconnect();
+              client2.disconnect();
+              done();
+            })
+          })
+        })
+      })
+    })
+  });
+
   describe('when the same user is connected multiple times', function () {
     it('notifies the other client logged in as sender about the message', function (done) {
       var client1 = io(url, options);
